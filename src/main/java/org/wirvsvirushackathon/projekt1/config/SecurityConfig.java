9a8584e2db02ecd8e98370990f2dfe14d604f6af
lkshrk <lukas@harke.me>
2020-03-20 11:24:04 +0100

    removed tls

--- src/main/java/org/wirvsvirushackathon/projekt1/config/SecurityConfig.java
+++ src/main/java/org/wirvsvirushackathon/projekt1/config/SecurityConfig.java
@@ -61,10 +61,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .requiresChannel()
-                    .anyRequest()
-                    .requiresSecure()
-                    .and()
                 .cors()
                     .and()
                 .sessionManagement()
